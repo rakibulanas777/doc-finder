0d50491a7a233c94ba4493dce1f4def53980e434
Rakibul islam Anas <rakibulanas777@gmail.com>
2023-07-15 02:32:23 +0600

    added filter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import Header from "../Component/Header";
-import { AiOutlinePlus } from "react-icons/ai";
+import {
+  AiOutlineArrowDown,
+  AiOutlineArrowUp,
+  AiOutlinePlus,
+} from "react-icons/ai";
 import { Link } from "react-router-dom";
 import doctor from "../assets/doctor.png";
 import axios from "axios";
@@ -10,6 +14,11 @@ import DoctorList from "./DoctorList";
 import PrimaryButton from "../Component/shared/PrimaryButton";
 const Home = () => {
   const [doctors, setDoctors] = useState([]);
+  const [value, getValue] = useState("all");
+  const [sort, setSort] = useState([]);
+  const handleSelect = (e) => {
+    getValue(e.target.value?.toLowerCase());
+  };
   const getUserData = async () => {
     try {
       const res = await axios.post(
@@ -22,7 +31,16 @@ const Home = () => {
         }
       );
       if (res.data.success) {
-        setDoctors(res.data.data);
+        if (value === "all") {
+          const doc = res.data.data;
+          setDoctors(doc);
+        } else {
+          const doc = res.data.data;
+          const match = doc.filter(
+            (Doctor) => Doctor.specialization.toLowerCase() === value
+          );
+          setDoctors(match);
+        }
       }
     } catch (error) {
       console.log(error);
@@ -31,24 +49,29 @@ const Home = () => {
 
   useEffect(() => {
     getUserData();
-  }, []);
+  }, [value]);
+
+  console.log(value);
   console.log(doctors);
+  // const matched = foods.filter((Food) =>
+  //   Food.catagory.toLowerCase().includes(search)
+  // );
   return (
     <div className="home">
       <Header />
       <div className="container mx-auto py-24 px-10 sm:px-8 md:px-6 lg:px-10">
         <div className="flex items-center justify-between text-black  pb-8">
-          <select className="select w-full max-w-xs border bg-white">
+          <select
+            className="select w-full max-w-xs border bg-white"
+            onChange={handleSelect}
+          >
             <option selected>All</option>
-            <option>chicken</option>
-            <option>foods</option>
-            <option>rice</option>
-            <option>ice-cream</option>
-            <option>Maggie</option>
+            <option>General Physician</option>
+            <option>Heart</option>
           </select>
           <button className="flex bg-white py-2 px-3 gap-2 items-center rounded-sm font-medium border-white">
             <span className=" font-medium">sort</span>
-            {/* {sort ? <AiOutlineArrowUp /> : <AiOutlineArrowDown />} */}
+            {sort ? <AiOutlineArrowUp /> : <AiOutlineArrowDown />}
           </button>
         </div>
 
